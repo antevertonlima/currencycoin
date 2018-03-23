6ac7c1d4cbf58a3371df7e640b944d29e4fdcd55
Everton Lima <antevertonlima@gmail.com>
2018-03-23 10:00:10 -0300

    Inicio da criação da strutura das apis de algoritimos e criptomoedas

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::get('/currency/{coin}', 'SomeController@index');
-Route::get('/calculator/{coin}/{hashrate}', 'BaseController@calculator')->name('api_calculator');
-Route::get('/payments/{coin}/{wallet}', 'BaseController@payments')->name('api_payments');
-Route::get('/general/{coin}/{wallet}', 'BaseController@general')->name('api_general');
+Route::apiResources([
+    'algo' => 'Api\AlgorithmController',
+    'coin' => 'Api\CoinController'
+]);
--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,6 @@ Route::get('/pool', 'SomeController@index')->name('pool');
 
 // rotas de gerenciamento (backend)
 Route::prefix('manager')->group(function () {    
-    Route::resource('algo', 'Manager\AlgorithmController', ['except' => [
-        'show'
-    ]]);
-    Route::resource('coin', 'Manager\CoinController', ['except' => [
-        'show'
-    ]]);
-    // Route::get('/algo', 'AlgorithmController@index')->name('algo');
-    // Route::get('/coin', 'CoinController@index')->name('coin');
+    Route::resource('algo', 'Manager\AlgorithmController', ['except' => ['show']]);
+    Route::resource('coin', 'Manager\CoinController', ['except' => ['show']]);
 });
